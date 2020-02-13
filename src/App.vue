75832ed62b6034352a2c2def33c83fec9f57cf06
daxong <ilove.h@outlook.com>
2020-02-13 10:52:22 +0800

    update:更新首页设计

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import index from './components/index.vue';
+import index from './components/Index.vue';
 import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
